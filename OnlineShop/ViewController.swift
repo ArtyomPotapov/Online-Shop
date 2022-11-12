57abbe025488f979a628f6c4037313ba2cb80944
ArtyomPotapov <artyom.potapov@icloud.com>
2022-11-12 18:26:15 +0300

    Initial Commit

---
+++ OnlineShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineShop
+//
+//  Created by Artyom Potapov on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
