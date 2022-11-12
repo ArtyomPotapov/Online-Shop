05e65608907d1e1e008bee3fdd46812552972bf7
ArtyomPotapov <artyom.potapov@icloud.com>
2022-11-12 21:29:54 +0300

    create cell and collection view classes

---
+++ OnlineShop/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  OnlineShop
+//
+//  Created by Artyom Potapov on 12.11.2022.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
